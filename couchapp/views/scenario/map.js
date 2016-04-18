031323238d1ff7db73f14d485ff2702061d40dc8
Darren Maczka <dmaczka@vt.edu>
2016-04-18 11:55:34 -0400

    fixed rewrites json

--- couchapp/views/scenario/map.js
+++ couchapp/views/scenario/map.js
@@ -2,6 +2,6 @@ function(doc) {
   if (doc.type == 'scenario') {
     emit([doc._id, 0], null)
   } else if (doc.type == 'comment') {
-    emit([doc.parent_id, 1], null)
+    emit(doc.ancestors.concat([1]), null)
   }
 }
