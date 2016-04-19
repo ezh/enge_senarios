59bed3ffb6a8f88e628d54911d1f103932fa3e46
Darren Maczka <dmaczka@vt.edu>
2016-04-18 20:40:57 -0400

    more styling

--- app/containers/AddScenario.js
+++ app/containers/AddScenario.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-let AddScenario = ({onAdd}) => {
+let AddScenario = ({onAdd, author}) => {
   let input;
  
   return (
-    <div className="input-group col-md-8">
+    <div className="input-group col-md-12">
+    <div className="col-md-8">
     <input type="text" className="form-control" placeholder="Add new scenario" ref={node => {
       input = node
     }}/>
-    <span className="input-group-btn">
+    </div>
+    <div className="col-md-2">
+    by {author}
+    </div>
+    <div className="input-group-btn col-md-2">
     <button className="btn btn-default" type="button" onClick={() => {
       onAdd(input.value);
       input.value = '';
     }}>Add</button>
-    </span>
-      </div>
+    </div>
+    </div>
   );
 }
 
