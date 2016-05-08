23330ac6bb209319a7ae04893cc2c234c4448a84
Darren Maczka <dmaczka@vt.edu>
2016-05-08 13:24:22 -0400

    add app title

--- app/components/Main.js
+++ app/components/Main.js
@@ -5,21 +5,26 @@ import UserSelection from './UserSelection'
 
 const Main = ({children}) => {
     return (
-      <div className="main-container">
-      <nav className="navbar navbar-default" role="navigation">
-          <div className="row">
-          <span className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-            <Link to="/">HOME</Link>
-          </span>
-         <span className="col-sm-7 col-sm-offset-6">
-           <UserSelection />
-         </span>
+<div className="main-container">
+  <nav className="navbar navbar-default" role="navigation">
+    <div className="row">
+      <div className="col-sm-6 col-sm-offset-2">
+        <h1>Culturally Relevant Problem Designer</h1>
       </div>
-        </nav>
-        <div className="container">
-          {children}
-        </div>
+      <div className="col-sm-4" style={{marginTop: 15}}>
+        <UserSelection />
       </div>
+    </div>
+    <div className="row">
+      <span className="col-sm-7 col-sm-offset-2">
+        <Link to="/">HOME</Link>
+      </span>
+    </div>
+  </nav>
+  <div className="container">
+    {children}
+  </div>
+</div>
     )
 };
 
